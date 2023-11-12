9b0f1934677fedb7e28930b889a854347dfe0636
Tan Nian Wei <tannianwei@aggienetwork.com>
2023-11-13 01:00:52 +0800

    update: nvim leap, zenmode, disabled mouse, mini-animate

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,3 +4,6 @@ require("config.lazy")
 -- Here's my own config I suppose?
 -- ref: https://github.com/neovim/neovim/issues/13474#issuecomment-739874877
 vim.wo.wrap = true
+
+-- Disable mouse
+vim.opt.mouse = ""
