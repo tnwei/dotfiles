3a1adcee52508ed06e71c052df8d245490968865
Tan Nian Wei <tannianwei@aggienetwork.com>
2023-11-14 21:10:44 +0800

    update: added 2nd layer for numpad in sofle

--- my-sofle-layout/keymap.c
+++ my-sofle-layout/keymap.c
@@ -27,16 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  Bspc   |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Rshift |
+ * |LShift|   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |Rshift|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  ENC  |    |  ENC  |------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
+ * | NULL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | NULL |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      | LGUI | LAlt | Space | /LCTL  /       \ENTER \  |RAISE | RAlt | Menu |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            | NULL | LGUI | LAlt | Space | /LCTL  /       \ENTER \  |RAISE | LOWER | Menu | NULL |
+ *            |      |      |      |      |/       /         \      \ |      |       |      |      |
  *            `----------------------------------'           '------''---------------------------'
  *
  * No CapsLock, prolly no required
@@ -48,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RSFT, 
     KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
     KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,     KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, 
-    KC_NO, KC_LGUI, KC_LALT, KC_SPC, KC_LCTL,       KC_ENT, MO(1), KC_RALT, KC_APP, KC_NO
+    KC_NO, KC_LGUI, KC_LALT, KC_SPC, KC_LCTL,       KC_ENT, MO(1), MO(2), KC_APP, KC_NO
 ),
 
 /* Layer 1
  * ,----------------------------------------.                    ,-----------------------------------------.
  * | ` / ~| F1   | F2   | F3   | F4   | F5   |                    | F6   | F7   | F8   | F9   | F10  | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |KC_MINS|     |KC_EQL|      |                    |      | PgUp |  Up  | PgDn | PrSrc|RSHIFT|
+ * |      |      |KC_MINS|     |KC_EQL|      |                    |      | PgUp |  Up  | PgDn | PrSrc|Rshift|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      | [ {  | \ |  | ] }  |      |-------.    ,-------|  Home| Left | Down | Right| Ins  |      |
  * |------+------+------+------+------+------|  ENC  |    | ENC   |------+------+------+------+------+------|
@@ -73,6 +73,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_NO, KC_LBRC, KC_BSLS, KC_RBRC, KC_NO,                   KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS, KC_NO,
     KC_TRNS, KC_F11, KC_F12, KC_NO,   KC_NO,   KC_NO, KC_NO,     KC_NO, KC_END,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+),
+
+/* Layer 2
+ * ,----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | LSFT |      |  7   |  8   |   9  |  0   |                    |  F10 |  F7  |  F8  |  F9  |      | RSFT |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |  4   |  5   |   6  |  -   |-------.    ,-------|  F11 |  F4  |  F5  |  F6  |      |      |
+ * |------+------+------+------+------+------|  ENC  |    | ENC   |------+------+------+------+------+------|
+ * |      |      |  1   |  2   |   3  |  =   |-------|    |-------|  F12 |  F1  |  F2  |  F3  |      |       |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            |      |      |      |      | / LCTL /       \      \  |      |      |      |      |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[2] = LAYOUT(
+    KC_NO,    KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO,                   KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_LSFT,  KC_NO,  KC_7,   KC_8,  KC_9,  KC_0,                    KC_F10, KC_F7, KC_F8, KC_F9, KC_NO, KC_RSFT,
+    KC_NO,    KC_NO,  KC_4,   KC_5,  KC_6,  KC_MINS,                 KC_F11, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO,
+    KC_NO,    KC_NO,  KC_1,   KC_2,  KC_3,  KC_EQL, KC_NO,    KC_NO, KC_F12, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO,
+    KC_NO,    KC_NO,  KC_NO,  KC_NO, KC_LCTL,                          KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
 
