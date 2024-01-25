af457bca88b83914c7fda65519b17d027e4b1f62
Tan Nian Wei <tnwei.ai@gmail.com>
2024-01-25 23:09:53 +0800

    update: added script and .desktop to toggle on/off laptop screen in hyprland

---
+++ scripts/toggle-monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+HYPRCTL_MONITORS=$(hyprctl monitors -j)
+MONITOR_NAMES=$(jq -r " .[] | .description" <<< $HYPRCTL_MONITORS)
+DEFAULT_MONITOR="eDP-1"
+
+if [[ $MONITOR_NAMES == *"$DEFAULT_MONITOR"* ]]; then
+    hyprctl keyword monitor "$DEFAULT_MONITOR,disable"
+    notify-send "Laptop monitor disabled"
+else
+    hyprctl keyword monitor "$DEFAULT_MONITOR,preferred,0x0,1.15"
+    notify-send "Laptop monitor reactivated"
+fi
