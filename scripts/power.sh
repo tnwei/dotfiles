0a468be31817e585d85fa0f410b10a53bfe7cfb7
Tan Nian Wei <tnwei.ai@gmail.com>
2024-02-01 00:52:11 +0800

    fix: power.sh scripts for wofi and rofi now

--- scripts/power.sh
+++ scripts/power.sh
@@ -4,11 +4,21 @@
 # Icon names from
 # https://specifications.freedesktop.org/icon-naming-spec/latest/ar01s04.html
 
+# Check if rofi or wofi is installed in the first place
+if command -v rofi &> /dev/null; then
+    LAUNCHER="rofi"
+elif command -v wofi &> /dev/null; then
+    LAUNCHER="wofi"
+else
+    echo "Error: Neither rofi nor wofi is found in the PATH."
+    exit 1
+fi
+
 # How to check if no params provided to a bash script
 # https://unix.stackexchange.com/a/25947
 if [ $# -eq 0 ]; then
 	ENTRIES=" Lock\n Logout\n Suspend\n Reboot\n Shutdown"
-	SELECTED=$(echo -e $ENTRIES | wofi --prompt "Power Menu" --width 120 --height 250 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+	SELECTED=$(echo -e $ENTRIES | $LAUNCHER --prompt "Power Menu" --width 120 --height 250 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 else
 	SELECTED=$1
 fi
