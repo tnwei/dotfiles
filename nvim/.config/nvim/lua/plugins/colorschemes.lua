797398f2dcad5058e968631345645b6615bdf76a
Tan Nian Wei <tnwei.ai@gmail.com>
2024-03-11 01:59:29 +0800

    update: added nvim colorschemes

---
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,16 @@
+return {
+    { "rebelot/kanagawa.nvim" },
+    { 
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000, -- make sure to load this before all the other start plugins
+    },
+    -- Set the startup colorscheme here
+      {
+        "LazyVim/LazyVim",
+        opts = {
+          colorscheme = "everforest",
+        },
+      },
+}
