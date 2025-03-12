60fae857bb18bbfdd9f47219a7de5b220efe8b4e
tnwei <tnwei.ai@gmail.com>
2025-03-12 12:22:30 +0800

    update: updated ranger config to preview js

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -295,7 +295,7 @@ handle_mime() {
             exit 1;;
 
         ## Text
-        text/* | */xml)
+        text/* | */xml | application/javascript)
             ## Syntax highlight
             if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
                 exit 2
