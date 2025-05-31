69fda5e32a34b9571fe1119b5390b7db34c6ad2c
tnwei <tnwei.ai@gmail.com>
2025-05-31 13:36:56 +0800

    chore: added rclone sync script

---
+++ scripts/sync-from-rclone.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Syncing ~/Documents ..."
+rclone sync remote:rclone-sync/Documents ~/Documents -L -P --interactive
+echo ""
+
+echo "Syncing ~/Music ..."
+rclone sync remote:rclone-sync/Music ~/Music -L -P --interactive
+echo ""
+
+echo "Syncing ~/wallpapers ..."
+rclone sync remote:rclone-sync/wallpapers ~/wallpapers -L -P --interactive
+echo ""
---
+++ scripts/sync-to-rclone.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Syncing ~/Documents ..."
+rclone sync ~/Documents remote:rclone-sync/Documents -L -P --interactive
+echo ""
+
+echo "Syncing ~/Music ..."
+rclone sync ~/Music remote:rclone-sync/Music -L -P --interactive
+echo ""
+
+echo "Syncing ~/wallpapers ..."
+rclone sync ~/wallpapers remote:rclone-sync/wallpapers -L -P --interactive
+echo ""
