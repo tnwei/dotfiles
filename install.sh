3b9db2d2f4b8fe9db59fdf1df55524b4fd3222d9
tnwei <tnwei.ai@gmail.com>
2025-09-23 02:28:54 +0800

    update: added vim-plug install script

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ declare -A GH_TOOLS
 GH_TOOLS["uv"]="curl -LsSf https://astral.sh/uv/install.sh | sh"
 GH_TOOLS["zoxide"]="curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh"
 GH_TOOLS["pnpm"]="curl -fsSL https://get.pnpm.io/install.sh | sh -"
+GH_TOOLS["vim-plug"]="curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 
 CORE_PKGS=(
